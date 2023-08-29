cb275b2955ed079357c20adcfc996a82f51c63c4
toast350 <canadian.frog350@gmail.com>
2023-08-29 03:45:05 -0500

    deleted test code

--- scripts/main.js
+++
@@ -1,7 +0,0 @@
-const myHeading = document.querySelector("h1");
-myHeading.textContent = "Hello world!";
-
-document.querySelector("html").addEventListener("click", function () {
-    alert("hehe :3");
-  });
-  
