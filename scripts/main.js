348f96f72066261e5a1ceb34cd162a45d966404f
toast350 <canadian.frog350@gmail.com>
2023-08-29 03:40:58 -0500

    made prompt when click on website

--- scripts/main.js
+++ scripts/main.js
@@ -1,2 +1,7 @@
 const myHeading = document.querySelector("h1");
 myHeading.textContent = "Hello world!";
+
+document.querySelector("html").addEventListener("click", function () {
+    alert("hehe :3");
+  });
+  
