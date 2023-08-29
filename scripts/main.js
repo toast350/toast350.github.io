0e1b89c4695e4f69c28ca12d817dcbeca0a49552
toast350 <canadian.frog350@gmail.com>
2023-08-29 04:50:27 -0500

    I forgot what i changed

--- scripts/main.js
+++ scripts/main.js
@@ -8,3 +8,27 @@ myImage.onclick = () => {
     myImage.setAttribute("src", "images/staring-cat.png");
   }
 };
+
+let myButton = document.querySelector("button");
+let myHeading = document.querySelector("h1");
+
+function setUserName() {
+  const myName = prompt("Please enter your name. (or something else, I don't care)");
+  if (!myName) {
+    setUserName()
+  } else {
+  localStorage.setItem("name", myName);
+  myHeading.textContent = `I have no idea what I am doing, ${myName}`;
+  }
+};
+
+if (!localStorage.getItem("name")) {
+  setUserName();
+} else {
+  const storedName = localStorage.getItem("name");
+  myHeading.textContent = `I have no idea what I am doing, ${storedName}`;
+};
+
+myButton.onclick = () => {
+  setUserName();
+};
